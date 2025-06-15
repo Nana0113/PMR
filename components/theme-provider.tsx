fe9b8e7a6c3d8154a03035d729396dde2c56a4d5
Nana0113 <Info@powermotionrescue.com>
2025-06-15 01:29:55 -0700

    Fix theme provider and add next-themes dependency

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,10 +3,15 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-interface ThemeProviderProps {
-  children: React.ReactNode
-}
-
-export function ThemeProvider({ children }: ThemeProviderProps) {
-  return <NextThemesProvider>{children}</NextThemesProvider>
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  )
 } 
